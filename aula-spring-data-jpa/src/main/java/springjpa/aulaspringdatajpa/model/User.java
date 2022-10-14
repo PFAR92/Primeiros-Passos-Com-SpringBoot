ff481888dd80c8f619fac87fb39ff3ac9304a05b
PFAR92 <paulofelipetj@hotmail.com>
2022-10-14 11:21:52 -0300

    14/10/22 11:20

---
+++ aula-spring-data-jpa/src/main/java/springjpa/aulaspringdatajpa/model/User.java
@@ -0,0 +1,42 @@
+package springjpa.aulaspringdatajpa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false)
+    private String username;
+    
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
